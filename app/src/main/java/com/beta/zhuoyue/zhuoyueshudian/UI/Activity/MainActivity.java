8fc660fe66d9e30ea5f7b42397cdf370ac17be98
Kevien <307572384@qq.com>
2018-09-18 18:19:27 +0800

    增加Xutils框架

--- app/src/main/java/com/beta/zhuoyue/zhuoyueshudian/UI/Activity/MainActivity.java
+++ app/src/main/java/com/beta/zhuoyue/zhuoyueshudian/UI/Activity/MainActivity.java
@@ -1,20 +1,19 @@
-package com.beta.zhuoyue.zhuoyueshudian;
+package com.beta.zhuoyue.zhuoyueshudian.UI.Activity;
 
 import android.app.Application;
 
 import org.xutils.x;
 
 /**
- * Created by Kevein on 2018/9/18.16:59
+ * Created by Kevein on 2018/9/18.16:58
  */
 
-public class MainActivity extends Application{
-
+public class MainActivity extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		x.Ext.init(this);
 		x.Ext.setDebug(false); //输出debug日志，开启会影响性能
-	}
 
+	}
 }
