62aaebb41fb960a99e989c4ab7dfaa52305aef68
Kevien <307572384@qq.com>
2018-09-25 13:54:01 +0800

    更新用户协议页面跳转功能

--- app/src/main/java/com/beta/zhuoyue/zhuoyueshudian/UI/Activity/UseragreeActivity.java
+++ app/src/main/java/com/beta/zhuoyue/zhuoyueshudian/UI/Activity/UseragreeActivity.java
@@ -1,8 +1,11 @@
 package com.beta.zhuoyue.zhuoyueshudian.UI.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.beta.zhuoyue.zhuoyueshudian.R;
 
@@ -11,10 +14,29 @@ import com.beta.zhuoyue.zhuoyueshudian.R;
  */
 
 public class UseragreeActivity extends Activity {
+	private ImageView exit;
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.useragreement);
 
+		initview();
 	}
+
+	private void initview() {
+		exit = (ImageView) findViewById(R.id.exit_useraggreement);
+	}
+
+	private void initaction() {
+		exit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(UseragreeActivity.this, RegistereActivity.class);
+				startActivity(intent);
+
+			}
+		});
+	}
+
 }
